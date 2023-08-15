1686a221559435b5029b4c960c237e0ea4524d6c
huangyue <huangyue@shsnc.com>
2023-08-15 15:12:46 +0800

    --【新增】修改

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
  * @Description: 项目配置
  * @Author: huangyue
  * @LastEditors: huangyue
- * @LastEditTime: 2023-08-15 13:49:28
+ * @LastEditTime: 2023-08-15 15:11:51
  */
 import { fileURLToPath, URL } from 'node:url'
 import { resolve } from 'path'
@@ -12,7 +12,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 //ant-design-vue 按需引入
 import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
-console.log(resolve(__dirname, 'pageages/index.js'))
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -49,14 +48,14 @@ export default defineConfig({
       // __dirname的值是vite.config.ts文件所在目录
       entry: resolve(__dirname, '/packages/index.js'),  // entry是必需的，因为库不能使用HTML作为入口。
       name: 'HyDesignVue', // 暴露的全局变量
-      fileName: 'hy-design-vue' // 输出的包文件名，默认是package.json的name选项
+      fileName: 'hy-design-vue', // 输出的包文件名，默认是package.json的name选项
     },
     rollupOptions: { // 自定义底层的Rollup打包配置
       // https://rollupjs.org/configuration-options/
       // 确保外部化处理那些你不想打包进库的依赖
       external: ['vue'],
       output: {
-        // format: 'es', // 默认es，可选 'amd' 'cjs' 'es' 'iife' 'umd' 'system'
+        // format: 'cjs', // 默认es，可选 'amd' 'cjs' 'es' 'iife' 'umd' 'system'
         exports: 'named', // https://rollupjs.org/configuration-options/#output-exports
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
@@ -87,3 +86,6 @@ export default defineConfig({
     }
   }
 })
+
+
+
